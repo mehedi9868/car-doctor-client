37da6856a7ade65402f8d10f5837ed74eb50b9d8
mehedi9868 <mehedi9868@gmail.com>
2023-05-13 18:31:11 +0600

    login and signup

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes'
+import AuthProvider from './providers/AuthProvider'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='max-w-5xl mx-auto'>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </React.StrictMode>,
 )
